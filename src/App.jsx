68ef3705c301689edb20de303902dc074ea28093
Raúl Armas <raularmasbx@gmail.com>
2024-05-05 21:30:07 -0500

    RMAB updates avance first commit ( aun falta completar el login)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './components/Home'
+import{Routes,Route} from 'react-router-dom'
+import Quotes from './components/Quotes';
+import BookDetails from './components/BookDetails';
+import NotFound from './components/NotFound';
+
+const App = () => {
+  return (
+		<div>
+		
+		</div>
+	);
+}
+
+export default App
+
