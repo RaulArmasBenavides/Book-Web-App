21dcff66c6e091cb61666835dee95e840ef9b0d7
Raúl Armas <raularmasbx@gmail.com>
2024-05-24 01:16:40 -0500

    logout implementation

--- src/books/components/Navbar.jsx
+++ src/books/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { startLogout } from '../../store/auth';
 
 const Navbar = () => {
+
+	const dispatch = useDispatch();
+
+    const onLogout = () => {
+        dispatch( startLogout() );
+    }
     return (
 		<div>
 			<div className='logo'>
@@ -17,6 +25,11 @@ const Navbar = () => {
 						</li>
 					</ul>
 				</nav>
+				<button className='bg-red-500 text-white px-4 py-2 rounded focus:outline-none hover:bg-red-600'
+				
+				onClick={ onLogout }>
+                Logout
+            </button>
 			</div>
 		</div>
 	);
